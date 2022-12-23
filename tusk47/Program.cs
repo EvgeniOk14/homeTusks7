5b3b7a61cdc816cd50ef27ed3bdff881f3300420
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-23 16:41:40 +0300

    added a new project: Tusk47

---
+++ tusk47/Program.cs
@@ -0,0 +1,18 @@
+﻿void InputArray(int[,] matrix)
+{
+  for(int i = 0; i < matrix.GetLength(0); i++)
+   {
+     for(int j = 0; j < matrix.GetLength(1); j++)
+     {
+        matrix[i, j] = new Random().Next(-1000, 1001);
+        Console.Write($"{matrix[i, j]} \t");
+     }
+   Console.WriteLine();
+   }
+} 
+Console.Clear();
+Console.WriteLine("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x=> int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputArray(matrix); 
+
